c0de4a1cbf804fa6442fc42217eae774c12dedf0
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-28 20:51:27 +0200

    Import all componebts

---
+++ bookstore/src/components/component.js
@@ -0,0 +1,8 @@
+import Books from './books/books';
+import Category from './categories/category';
+import NotFound from './error/notFound';
+import Book from './books/book';
+
+export {
+  Books, Category, NotFound, Book,
+};
