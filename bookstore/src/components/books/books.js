54270eefa64937ce1a38c25299740c791b7dce36
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-28 20:51:07 +0200

    Display all books in the books component

---
+++ bookstore/src/components/books/books.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './form';
+import Book from './book';
+import Header from './headers';
+
+const Books = () => (
+  <div className="home">
+    <Header />
+    <Book />
+    <Form />
+  </div>
+);
+
+export default Books;
