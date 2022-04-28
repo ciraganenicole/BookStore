8b35016f359e41854c5012464ce5c6e697789927
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-28 20:52:12 +0200

    Render all components

---
+++ bookstore/src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-quotes */
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Category, Books, NotFound } from './components/component';
+
+const App = () => (
+  <div>
+    <Router>
+      <Routes>
+        <Route path='/books' index element={<Books />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
+  </div>
+);
+
+export default App;
