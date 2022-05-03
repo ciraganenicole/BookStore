e9badef364cb492ee2823d996c526551db247ffa
Ciragane Nicole <ciraganenicole@gmail.com>
2022-05-03 13:38:52 +0200

    Create the redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { combineReducers, createStore } from 'redux';
+import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
+
+const rootReducer = combineReducers(bookReducer, categoryReducer);
+
+const store = createStore(rootReducer);
+
+export default store;
